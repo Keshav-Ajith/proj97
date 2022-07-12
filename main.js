6ef078d965c22dbe511d9af62995968f2f97445f
Keshav-Ajith <90075372+Keshav-Ajith@users.noreply.github.com>
2022-07-12 15:37:02 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function sendFF()
+{
+    window.location = "fast_food.html"
+}
+function sendCafe()
+{
+    window.location = "Cafe.html"
+}
+function sendAsian()
+{
+    window.location = "Asian.html"
+}
+function sendBuffet()
+{
+    window.location = "buffet.html"
+}
